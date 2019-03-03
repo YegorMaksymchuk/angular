25515c8c1cce465b43839927ffb6716334f7636f
Sergey <sergey.morenets@yandex.ua>
2019-03-03 11:30:37 +0200

    Task #13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Book} from './book';
+import {BookService} from './book.service';
 
 @Component({
   selector: 'app-root',
@@ -9,32 +10,8 @@ import {Book} from './book';
 export class AppComponent {
   books: Book[];
 
-  constructor() {
-    this.books = [
-      {
-        title: 'JavaScript: The Good Parts',
-        author: 'Douglas Crockford',
-        description: 'This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s\n' +
-          '      more reliable, readable, and maintainable',
-        pages: 2008,
-        year: 172
-      },
-      {
-        title: 'Mastering TypeScript',
-        author: 'Nathan Rozentals',
-        description: 'Build enterprise-ready, industrial strength web applications using TypeScript and leading\n' +
-          '      JavaScript frameworks',
-        pages: 2015,
-        year: 364
-      },
-      {
-        title: 'Switching to Angular 2',
-        author: 'Minko Gechev',
-        description: 'Start using TypeScript to supercharge your Angular 2 applications',
-        pages: 2016,
-        year: 254
-      }
-    ];
+  constructor(private bookService: BookService) {
+    this.books = bookService.getBooks();
   }
 
   isJavaScript(book: Book): boolean {
