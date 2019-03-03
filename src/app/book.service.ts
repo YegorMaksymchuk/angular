34c873039a9245090175c8ff0e49299726b8985a
Sergey <sergey.morenets@yandex.ua>
2019-03-03 11:30:57 +0200

    Task #13. Book service

---
+++ src/app/book.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {Book} from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  books: Book[];
+
+  constructor() {
+    this.books = [
+      {
+        title: 'JavaScript: The Good Parts',
+        author: 'Douglas Crockford',
+        description: 'This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s\n' +
+          '      more reliable, readable, and maintainable',
+        pages: 2008,
+        year: 172
+      },
+      {
+        title: 'Mastering TypeScript',
+        author: 'Nathan Rozentals',
+        description: 'Build enterprise-ready, industrial strength web applications using TypeScript and leading\n' +
+          '      JavaScript frameworks',
+        pages: 2015,
+        year: 364
+      },
+      {
+        title: 'Switching to Angular 2',
+        author: 'Minko Gechev',
+        description: 'Start using TypeScript to supercharge your Angular 2 applications',
+        pages: 2016,
+        year: 254
+      }
+    ];
+  }
+
+  getBooks(): Book[] {
+    return this.books;
+  }
+}
